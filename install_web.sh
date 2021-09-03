ad47d278e216990dc4b2d3070a8e6b7c44358f5a
sunilsharma2019 <56211916+sunilsharma2019@users.noreply.github.com>
2021-09-03 15:31:25 +0530

    install_web.sh

--- install_web.sh
+++ install_web.sh
@@ -1,5 +1,5 @@
 dnf install nginx
 systemctl start nginx
 systemctl enable nginx
-irewall-cmd --zone=public --permanent --add-service=http
+firewall-cmd --zone=public --permanent --add-service=http
 firewall-cmd --reload
